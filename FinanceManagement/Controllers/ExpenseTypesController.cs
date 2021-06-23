764a1fe34797196500c8677671767d1dbabc3796
Lucas Santana <lucas_377@hotmail.com>
2021-06-23 17:54:54 -0300

    ExpenseType controller with TempData to show messages

---
+++ FinanceManagement/Controllers/ExpenseTypesController.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanceManagement.Models.Entities;
+
+namespace FinanceManagement.Controllers
+{
+    public class ExpenseTypesController : Controller
+    {
+        private readonly Context _context;
+
+        public ExpenseTypesController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: ExpenseTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ExpenseTypes.ToListAsync());
+        }
+
+        // GET: ExpenseTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ExpenseTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ExpenseTypeId,Name")] ExpenseType expenseType)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Confirmation"] = expenseType.Name + " was created.";
+                _context.Add(expenseType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenseType);
+        }
+
+        // GET: ExpenseTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseType = await _context.ExpenseTypes.FindAsync(id);
+            if (expenseType == null)
+            {
+                return NotFound();
+            }
+            return View(expenseType);
+        }
+
+        // POST: ExpenseTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ExpenseTypeId,Name")] ExpenseType expenseType)
+        {
+            if (id != expenseType.ExpenseTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    TempData["Confirmation"] = expenseType.Name + " was edited.";
+                    _context.Update(expenseType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpenseTypeExists(expenseType.ExpenseTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenseType);
+        }
+
+        // POST: ExpenseTypes/Delete/5
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var expenseType = await _context.ExpenseTypes.FindAsync(id);
+            TempData["Confirmation"] = expenseType.Name + " was deleted.";
+            _context.ExpenseTypes.Remove(expenseType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpenseTypeExists(int id)
+        {
+            return _context.ExpenseTypes.Any(e => e.ExpenseTypeId == id);
+        }
+    }
+}
